517345919c0c91804a779314f4c21747ad5d3fff
Sean Bogolin <BogoGlitch@gmail.com>
2020-11-25 15:56:21 -0600

    Initial Commit - Header in Progress

---
+++ src/Components/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import Header from './Header'
+
+function App() {
+	return (
+		<div className='app'>
+			<Header />
+			{/* App Body */}
+			{/**  Sidebar  **/}
+			{/**  Stories  **/}
+			{/**  Feed  **/}
+		</div>
+	)
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyAvQ13s14Jf_reN40f_0UrGjcJMpnBJWCQ',
+	authDomain: 'fbook-c8fa6.firebaseapp.com',
+	databaseURL: 'https://fbook-c8fa6.firebaseio.com',
+	projectId: 'fbook-c8fa6',
+	storageBucket: 'fbook-c8fa6.appspot.com',
+	messagingSenderId: '1045188206944',
+	appId: '1:1045188206944:web:03f4f0d84ee5682f993e05',
+}
